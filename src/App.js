caca0b47b0d72d183216960214034b8dd2d97303
Marco Kwan <marcokwan10@gmail.com>
2022-03-29 22:10:15 -0400

    feat: player choice logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./component/Header";
 import Play from "./component/Play";
 import Result from "./component/Result";
 
 function App() {
+	const [playerChoice, setPlayerChoice] = useState(null);
+
+	const set = (choice) => {
+		console.log(choice);
+		setPlayerChoice(choice);
+	};
+
 	return (
 		<div>
 			<Header />
-			{/* <Play /> */}
-			<Result />
+			{!playerChoice ? <Play set={set} /> : <Result playerChoice={playerChoice} />}
 		</div>
 	);
 }
